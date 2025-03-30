2c6f3c81662dc2697459f1086206212ba7364877
Creaous <68230846+Creaous@users.noreply.github.com>
2025-03-31 00:33:09 +1030

    refactor: rename pages

--- src/app/t/email-changed/page.tsx
+++ src/app/t/email-changed/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="flex flex-col p-6 gap-3 justify-center items-center h-full flex-grow text-center">
+    <div className="flex flex-col p-6 gap-3 justify-center items-center h-screen flex-grow text-center">
       <div className="text-green-600">
         <CheckCircleIcon size={64} />
       </div>
