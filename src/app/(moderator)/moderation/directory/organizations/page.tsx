5f354915d0c1a07cc8497b03217e878222c11665
Creaous <68230846+Creaous@users.noreply.github.com>
2025-05-24 20:24:30 +0930

    feat: reusable data table component

--- src/app/(moderator)/moderation/directory/organizations/page.tsx
+++ src/app/(moderator)/moderation/directory/organizations/page.tsx
@@ -3,11 +3,11 @@ import { DataTable } from "@/components/data-table";
 import { db } from "@nexirift/db";
 
 export default async function Page() {
-  const data = await db.query.user.findMany();
+  const data = await db.query.organization.findMany();
 
   return (
     <main className="m-4 flex flex-col gap-2">
-      <h2 className="text-2xl font-bold">Users</h2>
+      <h2 className="text-2xl font-bold">Organizations</h2>
       <div className="flex items-center">
         <DataTable columns={columns} data={data} />
       </div>
