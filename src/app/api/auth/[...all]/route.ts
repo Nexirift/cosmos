2a63e466f384ab56f30d2a437911e157ce32aaf3
Creaous <68230846+Creaous@users.noreply.github.com>
2025-07-30 11:34:07 +0930

    fix: cors

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -2,3 +2,7 @@ import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
 export const { POST, GET } = toNextJsHandler(auth);
+
+export async function OPTIONS(request: Request) {
+  return new Response(null, { status: 204 });
+}
